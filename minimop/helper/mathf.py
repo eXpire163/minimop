074d25d5d5ac224c8271b4abe7f53c5e73a1d2a0
CWIEDEMA <christian.wiedemann@capgemini.com>
2017-05-29 14:53:50 +0200

    mathf round

--- minimop/helper/mathf.py
+++ minimop/helper/mathf.py
@@ -9,15 +9,14 @@ class Mathf(object):
         x_axis = [0, 1]
         y_axis = [xvalue, yvalue]
         result = np.interp(delta, x_axis, y_axis)
-        return result
+        return round(result, 3)
 
     @staticmethod
     def smoothstep(edge0, edge1, x):    
         # Scale, bias and saturate x to 0..1 range
         x = Mathf.clamp((x - edge0)/(edge1 - edge0), 0.0, 1.0)
         # Evaluate polynomial
-        return x*x*(3 - 2*x)
-
+        return round(x*x*(3 - 2*x), 3)
 
     @staticmethod
     def clamp(x, lowerlimit, upperlimit):        
